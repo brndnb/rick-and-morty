3cf201916d292115ace07cf1c1c8bb0623f6cb54
brndnb <116584527+brndnb@users.noreply.github.com>
2023-01-30 19:34:38 -0500

    homepage and about done

---
+++ src/pages/Homepage/Homepage.js
@@ -0,0 +1,42 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import "./Homepage.css"
+import CharacterCard from '../../components/CharacterCard/CharacterCard'
+function Homepage() {
+
+    //create state for characters
+    const [characters, setCharacters] = useState([])
+
+    useEffect(
+        ()=>{
+           axios.get("https://rickandmortyapi.com/api/character")
+           .then (response=>{
+                setCharacters(response.data.results)
+
+
+           })
+           .catch(err => console.log(err))
+
+        },[]
+
+
+    ) // end of useEffect
+
+
+  return (
+    <div className='home-container'>
+        <h1>Main Characters</h1>
+        <div className='characters-container'>
+        {
+            characters.map(item=><CharacterCard 
+                                  character={item}
+            />
+
+            )
+        }
+        </div>
+    </div>
+  )
+}
+
+export default Homepage
